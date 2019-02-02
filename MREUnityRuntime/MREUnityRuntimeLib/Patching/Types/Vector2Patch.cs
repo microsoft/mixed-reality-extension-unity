a79304fc34d796656f5d3908059a3b2ab35d9bb9
Steven Vergenz <steven.vergenz@microsoft.com>
2019-02-01 16:53:39 -0800

    Add new files

---
+++ MREUnityRuntime/MREUnityRuntimeLib/Patching/Types/Vector2Patch.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using MixedRealityExtension.Core.Types;
+using System;
+using UnityEngine;
+
+namespace MixedRealityExtension.Patching.Types
+{
+    public class Vector2Patch : IEquatable<Vector2Patch>, IPatchable
+    {
+        [PatchProperty]
+        public float? X { get; set; }
+
+        [PatchProperty]
+        public float? Y { get; set; }
+
+        public Vector2Patch()
+        {
+
+        }
+
+        public Vector2Patch(MWVector2 vector)
+        {
+            X = vector.X;
+            Y = vector.Y;
+        }
+
+        public Vector2Patch(Vector2 vector2)
+        {
+            X = vector2.x;
+            Y = vector2.y;
+        }
+
+        public Vector2Patch(Vector2Patch other)
+        {
+            if (other != null)
+            {
+                X = other.X;
+                Y = other.Y;
+            }
+        }
+
+        public bool Equals(Vector2Patch other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            else
+            {
+                return
+                    X.Equals(other.X) &&
+                    Y.Equals(other.Y);
+            }
+        }
+
+        public bool IsPatched()
+        {
+            return PatchingUtils.IsPatched(this);
+        }
+    }
+}
