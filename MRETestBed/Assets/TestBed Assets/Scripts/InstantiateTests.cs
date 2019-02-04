cd2e63ae0532c12625a8baf866024d9bc70fb8aa
Steven Vergenz <steven.vergenz@microsoft.com>
2019-02-04 15:24:43 -0800

    Rewrite test layout logic (#12)

--- MRETestBed/Assets/TestBed Assets/Scripts/InstantiateTests.cs
+++ MRETestBed/Assets/TestBed Assets/Scripts/InstantiateTests.cs
@@ -17,21 +17,31 @@ public class InstantiateTests : MonoBehaviour
             var testName= testNames[i];
 
             //Instantiate the test prefab and set parameters
-            var gameObject = GameObject.Instantiate(functionalTestPrefab, transform.parent);
-            gameObject.name = testName;
-            gameObject.transform.Find("Label").GetComponent<TextMesh>().text = testName;
-            gameObject.GetComponent<MREComponent>().SessionID = testName;
-            gameObject.GetComponent<MREComponent>().MREURL = MREURL + "?test=" + testName;
-            gameObject.GetComponent<MREComponent>().AppID = testName;
-            gameObject.GetComponent<MREComponent>().UserGameObject = transform.parent.Find("FPSControllerWithCursor").gameObject;
+            var test = GameObject.Instantiate(functionalTestPrefab, transform.parent);
+            test.name = testName;
+            test.transform.Find("Label").GetComponent<TextMesh>().text = testName;
+            var mre = test.GetComponentInChildren<MREComponent>();
+            if (mre != null)
+            {
+                mre.SessionID = testName;
+                mre.MREURL = MREURL + "?test=" + testName;
+                mre.AppID = testName;
+                mre.UserGameObject = transform.parent.Find("FPSControllerWithCursor").gameObject;
+            }
 
-            //Fan out the tests in a half-circle, scaled based on how many apps there are
-            float index = (((float)i) / (testNames.Length - 1)) - 0.5f;
-            float scale = 11.0f* Mathf.Sqrt(testNames.Length);
-            gameObject.transform.localPosition = new Vector3(Mathf.Sin(3.1415f*index), 0.0f, Mathf.Cos(3.1415f * index)-0.3f)*scale;
-            gameObject.transform.localEulerAngles= new Vector3(0.0f, index * 180.0f, 0.0f);
+            //Fan out the tests in a three-quarters circle, scaled based on how many apps there are
 
-
+            float threeQuarters = Mathf.PI * 1.5f;
+            // from -0.5 to 0.5
+            float index = (((float) i) / (testNames.Length - 1)) - 0.5f;
+            // find the radius of a three-quarters circle with n 12 meter tests
+            float radius = Mathf.Max(12, 12 * testNames.Length / threeQuarters);
+            test.transform.localPosition = radius * new Vector3(
+                Mathf.Sin(threeQuarters * index),
+                0.0f,
+                Mathf.Cos(threeQuarters * index)
+            );
+            test.transform.localRotation = Quaternion.LookRotation(test.transform.localPosition, Vector3.up);
         }
     }
     
