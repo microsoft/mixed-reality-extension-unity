c75f519acda343392ebe73db9779eda2b7552e61
Steven Vergenz <steven.vergenz@microsoft.com>
2019-02-08 13:36:30 -0800

    Support material alpha blending and cutout (#26)
    * Abstract material property mapping into plugin class
    
    * Add transparency fields
    
    * Fix testbed test names/order

---
+++ MREUnityRuntime/MREUnityRuntimeLib/Factories/DefaultMaterialApplicator.cs
@@ -0,0 +1,47 @@
+﻿using MixedRealityExtension.API;
+using MixedRealityExtension.Patching;
+using MixedRealityExtension.Patching.Types;
+using MixedRealityExtension.PluginInterfaces;
+using MixedRealityExtension.Util.Unity;
+using System;
+using Material = UnityEngine.Material;
+using MWMaterial = MixedRealityExtension.Assets.Material;
+using Texture = UnityEngine.Texture;
+
+namespace MixedRealityExtension.Factories
+{
+    /// <summary>
+    /// Default implementation of IMaterialPatcher. Only handles color and mainTexture property updates.
+    /// </summary>
+    public class DefaultMaterialPatcher : IMaterialPatcher
+    {
+        /// <inheritdoc />
+        public void ApplyMaterialPatch(Material material, MWMaterial patch)
+        {
+            if (patch.Color != null)
+                material.color = material.color.ToMWColor().ApplyPatch(patch.Color).ToColor();
+
+            if (patch.MainTextureId == Guid.Empty)
+                material.mainTexture = null;
+            else if (patch.MainTextureId != null)
+                material.mainTexture = (Texture)MREAPI.AppsAPI.AssetCache.GetAsset(patch.MainTextureId.Value);
+
+            if (patch.MainTextureOffset != null)
+                material.mainTextureOffset = material.mainTextureOffset.ToMWVector2().ApplyPatch(patch.MainTextureOffset).ToVector2();
+            if (patch.MainTextureScale != null)
+                material.mainTextureScale = material.mainTextureScale.ToMWVector2().ApplyPatch(patch.MainTextureScale).ToVector2();
+        }
+
+        /// <inheritdoc />
+        public MWMaterial GeneratePatch(Material material)
+        {
+            return new MWMaterial()
+            {
+                Color = new ColorPatch(material.color),
+                MainTextureId = MREAPI.AppsAPI.AssetCache.GetId(material.mainTexture),
+                MainTextureOffset = new Vector2Patch(material.mainTextureOffset),
+                MainTextureScale = new Vector2Patch(material.mainTextureScale)
+            };
+        }
+    }
+}
---
+++ MREUnityRuntime/MREUnityRuntimeLib/PluginInterfaces/IMaterialPatcher.cs
@@ -0,0 +1,25 @@
+﻿using Material = UnityEngine.Material;
+using MWMaterial = MixedRealityExtension.Assets.Material;
+
+namespace MixedRealityExtension.PluginInterfaces
+{
+    /// <summary>
+    /// Responsible for translating between the host's material properties and the API properties
+    /// </summary>
+    public interface IMaterialPatcher
+    {
+        /// <summary>
+        /// Apply the patch from the app to the material
+        /// </summary>
+        /// <param name="material">An instance of the default MRE material provided on load</param>
+        /// <param name="patch">The update from the app. Unmodified properties will be null.</param>
+        void ApplyMaterialPatch(Material material, MWMaterial patch);
+
+        /// <summary>
+        /// Generate an API patch from the Unity material's current state
+        /// </summary>
+        /// <param name="material">An instance of the default MRE material provided on load</param>
+        /// <returns>A full definition of the given material</returns>
+        MWMaterial GeneratePatch(Material material);
+    }
+}
