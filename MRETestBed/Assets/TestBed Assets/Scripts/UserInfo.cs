d095d03de74fb5ba2697f685b354053d38cdbc2c
Eric Anderson <eanders@microsoft.com>
2019-03-01 14:12:12 -0800

    Add some attach points to MRETestBed character

--- MRETestBed/Assets/TestBed Assets/Scripts/UserInfo.cs
+++ MRETestBed/Assets/TestBed Assets/Scripts/UserInfo.cs
@@ -28,8 +28,32 @@ internal class UserInfo : IUserInfo
     public event MWEventHandler BeforeAvatarDestroyed;
     public event MWEventHandler AfterAvatarCreated;
 
+    private static Transform FindChildRecursive(Transform parent, string name)
+    {
+        Transform transform = parent.Find(name);
+        if (transform != null)
+        {
+            return transform;
+        }
+        for (int i = 0; i < parent.childCount; ++i)
+        {
+            transform = FindChildRecursive(parent.GetChild(i), name);
+            if (transform != null)
+            {
+                return transform;
+            }
+        }
+        return null;
+    }
+
     public Transform GetAttachPoint(string attachPointName)
     {
-        return UserGO.transform;
+        string socketName = $"socket-{attachPointName}";
+        Transform socket = FindChildRecursive(UserGO.transform, socketName);
+        if (socket == null)
+        {
+            socket = UserGO.transform;
+        }
+        return socket;
     }
 }
