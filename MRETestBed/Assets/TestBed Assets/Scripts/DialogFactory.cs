e134237516d48e471aa16c51dc280fa6ff46faa1
Steven Vergenz <steven.vergenz@microsoft.com>
2019-09-19 16:58:42 -0700

    Start implementing in testbed

---
+++ MRETestBed/Assets/TestBed Assets/Scripts/DialogFactory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MixedRealityExtension.PluginInterfaces;
+using System;
+
+public class DialogFactory : MonoBehaviour, IDialogFactory
+{
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	public void ShowDialog(string text, bool allowInput, Action<bool, string> callback)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnOk()
+	{
+		Debug.Log("Ok");
+	}
+
+	public void OnCancel()
+	{
+		Debug.Log("Cancel");
+	}
+}
---
+++ MRETestBed/Assets/TestBed Assets/Scripts/Player/MreInputModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine.EventSystems;
+
+public class MreInputModule : PointerInputModule
+{
+	protected override MouseState GetMousePointerEventData()
+	{
+
+	}
+
+	public override void Process()
+	{
+		base.Process();
+	}
+}
