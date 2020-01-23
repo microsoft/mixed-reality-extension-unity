368e9c905e18e5e10b38be5c42d608554efe4cd1
Steven Vergenz <steven.vergenz@microsoft.com>
2020-01-22 16:46:01 -0800

    Version bump

--- MREUnityRuntime/MREUnityRuntimeLib/Constants.cs
+++ MREUnityRuntime/MREUnityRuntimeLib/Constants.cs
@@ -45,7 +45,7 @@ namespace MixedRealityExtension
 		 * This matches major.minor from the package version number, and is updated as part of the manual
 		 * SDK release procedures.
 		 */
-		internal const string CurrentClientVersion = "0.14";
+		internal const string CurrentClientVersion = "0.15";
 
 		/*
 		 * Minimum Supported SDK version
