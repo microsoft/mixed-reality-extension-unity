98cd220de5a0340a439a1ab3fbc59e41b40b4f6d
Steven Vergenz <steven.vergenz@microsoft.com>
2020-03-09 10:01:58 -0700

    [Unity] Add an emissive color to materials (#125)
    * Add emissive color
    
    * Update shader, patcher
    
    * Don't count triangles in gltf loader
    
    * Re-add license headers
    
    * More headers
    
    * Add emissive texture
    
    * Update gltf loader

--- MREUnityRuntime/MREUnityRuntimeLib/Assets/Definitions.cs
+++ MREUnityRuntime/MREUnityRuntimeLib/Assets/Definitions.cs
@@ -132,6 +132,26 @@ namespace MixedRealityExtension.Assets
 		public Vector2Patch MainTextureScale;
 
 		/// <summary>
+		/// The lighting-independent color
+		/// </summary>
+		public ColorPatch EmissiveColor;
+
+		/// <summary>
+		/// The ID of the main texture asset
+		/// </summary>
+		public Guid? EmissiveTextureId;
+
+		/// <summary>
+		/// Offset the texture by this amount as a fraction of the resolution
+		/// </summary>
+		public Vector2Patch EmissiveTextureOffset;
+
+		/// <summary>
+		/// Scale the texture by this amount in each axis
+		/// </summary>
+		public Vector2Patch EmissiveTextureScale;
+
+		/// <summary>
 		/// How this material should treat the color/texture alpha channel
 		/// </summary>
 		public AlphaMode? AlphaMode;
