dcbdf12dd30ccb2e195ed1d0d0c9ab45d8a5b252
Steven Vergenz <steven.vergenz@microsoft.com>
2020-04-27 11:19:00 -0700

    Change MRE to use TMP assets

---
+++ MRETestBed/Assets/TestBed Assets/Scripts/TmpTextFactory.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using MixedRealityExtension.Core.Interfaces;
+using MixedRealityExtension.PluginInterfaces;
+using TMPro;
+using UnityEngine;
+
+public class TmpTextFactory : ITextFactory
+{
+	public TMP_FontAsset SerifFont;
+	public TMP_FontAsset SansSerifFont;
+
+	public IText CreateText(IActor actor)
+	{
+		var actorGo = actor.GameObject;
+		var tmp = actorGo.GetComponent<TextMeshPro>();
+		if (tmp == null)
+		{
+			tmp = actorGo.AddComponent<TextMeshPro>();
+		}
+
+		return new TmpText(this, tmp);
+	}
+}
