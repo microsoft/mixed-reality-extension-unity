1f618bfa48908a45602f71eedccc1c0028068d99
Steven Vergenz <steven.vergenz@microsoft.com>
2020-04-28 11:51:01 -0700

    [Unity] Add more font types (#139)
    * Add TextMesh Pro
    
    * Change MRE to use TMP assets
    
    * Add font atlases, monospace and handwritten fonts
    
    * Implement new fonts

---
+++ MRETestBed/Assets/TestBed Assets/Scripts/TmpTextFactory.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using MixedRealityExtension.Core.Interfaces;
+using MixedRealityExtension.PluginInterfaces;
+using TMPro;
+using UnityEngine;
+
+public class TmpTextFactory : ITextFactory
+{
+	public TMP_FontAsset DefaultFont;
+	public TMP_FontAsset SerifFont;
+	public TMP_FontAsset SansSerifFont;
+	public TMP_FontAsset MonospaceFont;
+	public TMP_FontAsset CursiveFont;
+
+	public IText CreateText(IActor actor)
+	{
+		var actorGo = actor.GameObject;
+		var tmp = actorGo.GetComponent<TextMeshPro>();
+		if (tmp == null)
+		{
+			tmp = actorGo.AddComponent<TextMeshPro>();
+		}
+
+		return new TmpText(this, tmp);
+	}
+}
