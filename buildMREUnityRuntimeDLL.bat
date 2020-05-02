5943010bde4f2984d3e8ea38a118a57054ac6495
badev <dramirez@soulrivers.com>
2020-05-01 23:07:05 -0400

    Update buildMREUnityRuntimeDLL.bat (#129)
    * Update buildMREUnityRuntimeDLL.bat
    
    Updated for VS 2019
    
    * Update buildMREUnityRuntimeDLL.bat
    
    Updated to "2017 or later" per review

--- buildMREUnityRuntimeDLL.bat
+++ buildMREUnityRuntimeDLL.bat
@@ -3,20 +3,20 @@
 
 for /f "usebackq tokens=*" %%i in (`"%~dp0tools\vswhere" -products * -requires Microsoft.Component.MSBuild -property installationPath`)	 do (
   set VSInstallDir=%%i
-  if exist "%%i\MSBuild\15.0\Bin\MSBuild.exe" goto FoundVisualStudio
+  if exist "%%i\MSBuild\Current\Bin\MSBuild.exe" goto FoundVisualStudio
 )
-echo MSBuild not found - please install Visual Studio 2017
+echo MSBuild not found - please install Visual Studio 2017 or later
 pause
 exit /b 1
 
 :FoundVisualStudio
 echo Building Mixed Reality Unity Runtime DLL
 set ErrorString=Failed Visual Studio build
-"%VSInstallDir%\MSBuild\15.0\Bin\MSBuild.exe" "%~dp0MREUnityRuntime\MREUnityRuntime.sln" /p:Configuration=Release /p:Platform="Any CPU"
+"%VSInstallDir%\MSBuild\Current\Bin\MSBuild.exe" "%~dp0MREUnityRuntime\MREUnityRuntime.sln" /p:Configuration=Release /p:Platform="Any CPU"
 
 if errorlevel 1 (
   echo BUILD FAILED: %ErrorString%
-  echo Make sure you have Visual Studio 2017, and Visual Studio's Unity Tools installed
+  echo Make sure you have Visual Studio 2017 or later, and Visual Studio's Unity Tools installed
   pause
   exit /b 1
 )
