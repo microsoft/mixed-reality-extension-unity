88cad74ee171bca67c30a5a47ea121909953ceab
jabenk <40789724+jabenk@users.noreply.github.com>
2020-05-05 12:51:18 +0200

    Jabenk/physics merge (#148)
    * [Unity] Animations overhaul part 2 (#135)
    
    * Update animation target IDs
    
    * Start adding new network types
    
    * Start parsing new animation messages and types
    
    * Tweaks
    
    * Experiment with animation patching strategy
    
    * More animation work
    
    * Start end-to-end impl
    
    * Don't run update if speed is 0
    
    * Clarify role of one-last-update flag
    
    * Fix some bugs
    
    * Add more WriteToPatch methods
    
    * Use CubicBezier for interpolation, add value type interpolation, keyframe lookup optimization
    
    * Add ability to read out of actors (in-progress)
    
    * Support partial patch generation, start implicit start frame work
    
    * Rewrite+simplify keyframe search
    
    * Capture implicit frame for relative tracks
    
    * Support relative animations, destroy anims when all targets destroyed
    
    * Better handling of unloaded anim data
    
    * Clean up unused imports
    
    * Fix edge case where both frames have same time
    
    * Minor comprehension cleanup
    
    * Move orphaned anim detection to app
    
    * Support realtime keyframe values
    
    * Clean up anims on disconnect
    
    * Remove debug line
    
    * Minor cleanup
    
    * Blend multiple anims targeting same object
    
    * Fix anim blending, add IsPlaying property
    
    * Reduce garbage allocations
    
    * Looping relative anims, better token pooling
    
    * Fix comment
    
    * Reduce garbage
    
    * More GC alloc reductions
    
    * More GC alloc reduction
    
    * Correctly override step interpolation
    
    * Default to linear easing, fix xfrm patching with RB
    
    * Move finished state transition to client, cleanup work
    
    * Version bump: 0.17
    
    * Don't use conditional accessor on MonoBehaviors (#137)
    
    * Adapt to the merge from the red branch
    
    * Add bounciness and frictions
    
    * Add more missing pieces for bounciness and fricitons
    
    * [Unity] Add more font types (#139)
    
    * Add TextMesh Pro
    
    * Change MRE to use TMP assets
    
    * Add font atlases, monospace and handwritten fonts
    
    * Implement new fonts
    
    * Add missing font associations. (#142)
    
    * Jabenk/pr (#140)
    
    * Add bounciness and frictions
    
    * Add more missing pieces for bounciness and fricitons
    
    * PR fixes
    
    * Update UnityGLTF, enable threaded loading (#143)
    
    * Update buildMREUnityRuntimeDLL.bat (#129)
    
    * Update buildMREUnityRuntimeDLL.bat
    
    Updated for VS 2019
    
    * Update buildMREUnityRuntimeDLL.bat
    
    Updated to "2017 or later" per review
    
    * current dev state to predict collisions
    
    * Currect collision dev state
    
    * First version of smooth interpolation (but more debuging is needed
    
    * Limit concurrent web requests (#146)
    
    * Limit texture and audio clip loads to 4 at once
    
    * Add code comments
    
    * Decrement earlier
    
    * Add try/catch
    
    * Wrap importer in a using statement
    
    * No need for interlocked. Code is not multithreaded
    
    * Make this less hacky
    
    * Some fixes, but probably the velocity approximation is not correct
    
    * A rather stable state where the 2 tests work well
    
    * Add a better estimate for the implicit velocit from the transform
    
    Co-authored-by: Steven Vergenz <steven.vergenz@microsoft.com>
    Co-authored-by: Eric Anderson <eanders@microsoft.com>
    Co-authored-by: badev <dramirez@soulrivers.com>

--- MREUnityRuntime/MREUnityRuntimeLib/Core/PhysicsBridge.cs
+++ MREUnityRuntime/MREUnityRuntimeLib/Core/PhysicsBridge.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace MixedRealityExtension.Core
 {
@@ -10,63 +11,92 @@ namespace MixedRealityExtension.Core
 	{
 		public class SnapshotBuffer
 		{
+			/// <summary>
+			/// this stores the snapshots received from all other clients, each client sends the owned updates of the actors (RBs)
+			/// </summary>
+			/// If one wants an updated for a remote body then needs to go though all these separate updates and one of the updates from one client we should find the actor ID
 			Dictionary<Guid, Snapshot> _snapshots = new Dictionary<Guid, Snapshot>();
 
 			public void addSnapshot(Snapshot snapshot)
 			{
 				// currently keep only the latest snapshot from same source
 
-				if (_snapshots.ContainsKey(snapshot.SourceId))
+				if (_snapshots.ContainsKey(snapshot.SourceAppId))
 				{
-					var oldSnapshot = _snapshots[snapshot.SourceId];
+					var oldSnapshot = _snapshots[snapshot.SourceAppId];
 
 					if (snapshot.Time > oldSnapshot.Time)
 					{
-						_snapshots[snapshot.SourceId] = snapshot;
+						_snapshots[snapshot.SourceAppId] = snapshot;
 					}
 				}
 				else
 				{
-					_snapshots.Add(snapshot.SourceId, snapshot);
+					_snapshots.Add(snapshot.SourceAppId, snapshot);
 				}
 			}
 
-			public Snapshot.Transform getTransform(Guid rbId)
+			public Snapshot.SnapshotTransform getTransform(Guid rbId, out float timeOfSnapshot)
 			{
+				timeOfSnapshot = -float.MaxValue;
 				foreach (var s in _snapshots.Values)
 				{
-					foreach (var t in s.Transforms)
+					foreach (var t in s.snapshotTransforms)
 					{
 						if (t.Id == rbId)
 						{
-							return t.Transform;
+							timeOfSnapshot = s.Time;
+							return t.sTransform;
 						}
 					}
 				}
-
 				return null;
 			}
 		}
 
 		public class Snapshot
 		{
-			public class Transform
+			public class SnapshotTransform
 			{
 				public UnityEngine.Vector3 Position;
 				public UnityEngine.Quaternion Rotation;
 			}
 
-			public class TrasformInfo
+			public class SnapshotTrasformInfo
 			{
 				public Guid Id;
-				public Transform Transform = new Transform();
+				public SnapshotTransform sTransform = new SnapshotTransform();
 			}
 
-			public Guid SourceId;
+			public Guid SourceAppId;
 
 			public float Time;
 
-			public List<TrasformInfo> Transforms = new List<TrasformInfo>();
+			public List<SnapshotTrasformInfo> snapshotTransforms = new List<SnapshotTrasformInfo>();
+		}
+
+		/// <summary>
+		/// to monitor the collisions between frames this we need to store per contact pair. 
+		/// </summary>
+		public class CollisionMonitorInfo
+		{
+			public float timeFromStartCollision = 0.0f;
+			public float relativeDistance = float.MaxValue;
+			/// how much we interpolate between the jitter buffer and the simulation
+			public float keyframedInterpolationRatio = 0.0f; 
+		}
+
+		/// <summary>
+		///  for interactive collisions we need to store the implicit velocities and other stuff to know
+		/// </summary>
+		public class CollisionSwitchInfo
+		{
+			public UnityEngine.Vector3 startPosition;
+			public UnityEngine.Quaternion startOrientation;
+			public UnityEngine.Vector3 linearVelocity;
+			public UnityEngine.Vector3 angularVelocity;
+			public Guid rigidBodyId;
+			public CollisionMonitorInfo monitorInfo;
 		}
 
 		private class RigidBodyInfo
@@ -76,13 +106,21 @@ namespace MixedRealityExtension.Core
 				Id = id;
 				RigidBody = rb;
 				Ownership = ownership;
+				lastTimeKeyFramedUpdate = 0.0f;
+				lastValidLinerVelocity.Set(0.0f, 0.0f, 0.0f);
+				lastValidAngularVelocity.Set(0.0f, 0.0f, 0.0f);
 			}
 
-			public Guid  Id;
+			public Guid Id;
 
 			public UnityEngine.Rigidbody RigidBody;
 
-			public bool Ownership;
+			/// these 3 fields are used to store the actual velocities
+			public float lastTimeKeyFramedUpdate;
+			public UnityEngine.Vector3 lastValidLinerVelocity;
+			public UnityEngine.Vector3 lastValidAngularVelocity;
+
+			public bool Ownership; ///< true if this rigid body is owned by this client
 		}
 
 		public Guid _appId;
@@ -94,6 +132,12 @@ namespace MixedRealityExtension.Core
 
 		private SnapshotBuffer _snapshotBuffer = new SnapshotBuffer();
 
+		/// when we update a body we compute the implicit velocity. This velocity is needed, in case of collisions to switch from kinematic to kinematic=false
+		List<CollisionSwitchInfo> _switchCollisionInfos = new List<CollisionSwitchInfo>();
+
+		/// <todo> the input should be GuidXGuid since one body could collide with multiple other, or one collision is enough (?)
+		Dictionary<Guid, CollisionMonitorInfo> _monitorCollisionInfo = new Dictionary<Guid, CollisionMonitorInfo>();
+
 		public PhysicsBridge()
 		{
 		}
@@ -102,7 +146,7 @@ namespace MixedRealityExtension.Core
 
 		public void addRigidBody(Guid id, UnityEngine.Rigidbody rigidbody, bool ownership)
 		{
-			UnityEngine.Debug.Assert(!_rigidBodies.ContainsKey(id), "PhysicsBridge already ahs an entry for rigid body with specified ID.");
+			UnityEngine.Debug.Assert(!_rigidBodies.ContainsKey(id), "PhysicsBridge already has an entry for rigid body with specified ID.");
 
 			_rigidBodies.Add(id, new RigidBodyInfo(id, rigidbody, ownership));
 
@@ -167,7 +211,16 @@ namespace MixedRealityExtension.Core
 		public void FixedUpdate(UnityEngine.Transform rootTransform)
 		{
 			// physics rigid body management
-			// set transforms/velocities for keyframed bodies
+			// set transforms/velocities for key framed bodies
+			float DT = UnityEngine.Time.fixedDeltaTime;
+			float halfDT = 0.5f * DT;
+			float invDT = (1.0f / DT);
+			const float startInterpolatingBack = 0.6f; // time in seconds
+			const float endInterpolatingBack = 2.0f; // time in seconds
+			const float invInterpolationTimeWindows = 1.0f / (endInterpolatingBack - startInterpolatingBack);
+
+			// delete all the previous collisions
+			_switchCollisionInfos.Clear();
 
 			foreach (var rb in _rigidBodies.Values)
 			{
@@ -175,16 +228,192 @@ namespace MixedRealityExtension.Core
 				{
 					continue;
 				}
+				float timeOfSnapshot;
 
-				Snapshot.Transform transform = _snapshotBuffer.getTransform(rb.Id);
+				Snapshot.SnapshotTransform transform = _snapshotBuffer.getTransform(rb.Id, out timeOfSnapshot);
 
 				if (transform != null)
 				{
-					rb.RigidBody.isKinematic = true;
-					rb.RigidBody.transform.position = /*rootTransform.position +*/ rootTransform.TransformPoint(transform.Position);
-					rb.RigidBody.transform.rotation = rootTransform.rotation * transform.Rotation;
+					var collisionInfo = new CollisionSwitchInfo();
+
+					collisionInfo.startPosition = rb.RigidBody.transform.position;
+					collisionInfo.startOrientation = rb.RigidBody.transform.rotation;
+					collisionInfo.rigidBodyId = rb.Id;
+
+					if (_monitorCollisionInfo.ContainsKey(rb.Id))
+					{
+						rb.RigidBody.isKinematic = false;
+						collisionInfo.monitorInfo = _monitorCollisionInfo[rb.Id];
+						collisionInfo.monitorInfo.timeFromStartCollision += DT;
+						collisionInfo.linearVelocity = rb.RigidBody.velocity;
+						collisionInfo.angularVelocity = rb.RigidBody.angularVelocity;
+#if MRE_PHYSICS_DEBUG
+						Debug.Log(" Remote body: " + rb.Id.ToString() + " is dynamic since:"
+							+ collisionInfo.monitorInfo.timeFromStartCollision + " relative distance:" + collisionInfo.monitorInfo.relativeDistance
+							+ " interpolation:" + collisionInfo.monitorInfo.keyframedInterpolationRatio);
+#endif
+
+						// if time passes by then make a transformation between key framed and dynamic
+						// but only change the positions not the velocity
+						if (collisionInfo.monitorInfo.keyframedInterpolationRatio > 0.001f)
+						{
+							// interpolate between key framed and dynamic transforms
+							float t = collisionInfo.monitorInfo.keyframedInterpolationRatio;
+							UnityEngine.Vector3 keyFramedPos = rootTransform.TransformPoint(transform.Position), interpolatedPos;
+							UnityEngine.Quaternion keyFramedOrientation = rootTransform.rotation * transform.Rotation, interpolatedQuad;
+							interpolatedPos = t * keyFramedPos + (1.0f - t) * rb.RigidBody.transform.position;
+							interpolatedQuad = UnityEngine.Quaternion.Slerp(keyFramedOrientation, rb.RigidBody.transform.rotation, t);
+							rb.RigidBody.transform.position = interpolatedPos;
+							rb.RigidBody.transform.rotation = interpolatedQuad;
+						}
+					}
+					else
+					{
+						// no previous collision, keep key framed
+						UnityEngine.Vector3 newPosition = rootTransform.TransformPoint(transform.Position);
+						UnityEngine.Quaternion newOrientation = rootTransform.rotation * transform.Rotation;
+						rb.RigidBody.isKinematic = true;
+						// if there is a really new update then also store the implicit velocity
+						if (rb.lastTimeKeyFramedUpdate < timeOfSnapshot)
+						{
+							// <todo> for long running times this could be a problem 
+							float invUpdateDT = 1.0f/(timeOfSnapshot - rb.lastTimeKeyFramedUpdate);
+							rb.lastValidLinerVelocity = (newPosition - collisionInfo.startPosition) * invUpdateDT;
+							rb.lastValidAngularVelocity = (UnityEngine.Quaternion.Inverse(collisionInfo.startOrientation)
+							 * newOrientation).eulerAngles * invUpdateDT;
+						}
+						rb.RigidBody.transform.position = newPosition;
+						rb.RigidBody.transform.rotation = newOrientation;
+						rb.lastTimeKeyFramedUpdate = timeOfSnapshot;
+
+						collisionInfo.linearVelocity = rb.lastValidLinerVelocity;
+						collisionInfo.angularVelocity = rb.lastValidAngularVelocity;
+						collisionInfo.monitorInfo = new CollisionMonitorInfo();
+#if MRE_PHYSICS_DEBUG
+						Debug.Log(" Remote body: " + rb.Id.ToString() + " is key framed:");
+#endif
+					}
+					// <todo> add more filtering here to cancel out unnecessary items,
+					// but for small number of bodies should be OK
+					_switchCollisionInfos.Add(collisionInfo);
 				}
 			}
+
+			// clear here all the monitoring since we will re add them
+			_monitorCollisionInfo.Clear();
+
+			// test collisions of each owned body with each not owned body
+			foreach (var rb in _rigidBodies.Values)
+			{
+				if (rb.Ownership)
+				{
+					// <todo> test here all the remote-owned collisions and those should be turned to dynamic again.
+					foreach (var remoteBodyInfo in _switchCollisionInfos)
+					{
+						var remoteBody = _rigidBodies[remoteBodyInfo.rigidBodyId].RigidBody;
+						var comDist = (remoteBody.transform.position - rb.RigidBody.transform.position).magnitude;
+
+						var remoteHitPoint = remoteBody.ClosestPointOnBounds(rb.RigidBody.transform.position);
+						var ownedHitPoint = rb.RigidBody.ClosestPointOnBounds(remoteBody.transform.position);
+
+						var remoteRelativeHitP = (remoteHitPoint - remoteBody.transform.position);
+						var ownedRelativeHitP = (ownedHitPoint - rb.RigidBody.transform.position);
+
+						var radiousRemote = 1.3f * remoteRelativeHitP.magnitude;
+						var radiusOwnedBody = 1.3f * ownedRelativeHitP.magnitude;
+
+						var totalDistance = radiousRemote + radiusOwnedBody + 0.0001f; // avoid division by zero
+
+						// project the linear velocity of the body
+						var projectedOwnedBodyPos = rb.RigidBody.transform.position + rb.RigidBody.velocity * DT;
+						var projectedComDist = (remoteBody.transform.position - projectedOwnedBodyPos).magnitude;
+
+#if MRE_PHYSICS_DEBUG
+						Debug.Log("prprojectedComDistoj: " + projectedComDist + " comDist:" + comDist
+							+ " totalDistance:" + totalDistance + " remote body pos:" + remoteBodyInfo.startPosition.ToString()
+							+ "input lin vel:" + remoteBodyInfo.linearVelocity + " radiousRemote:" + radiousRemote +
+							" radiusOwnedBody:" + radiusOwnedBody);
+#endif
+
+						var collisionMonitorInfo = remoteBodyInfo.monitorInfo;
+						float lastApproxDistance = Math.Min(comDist, projectedComDist);
+						collisionMonitorInfo.relativeDistance = lastApproxDistance / totalDistance;
+
+						bool addToMonitor = false;
+						bool isWithinCollisionRange = (projectedComDist < totalDistance || comDist < totalDistance || addToMonitor);
+						float timeSinceCollisionStart = collisionMonitorInfo.timeFromStartCollision;
+
+						// unconditionally add to the monitor stream if this is a reasonable collision and we are only at the beginning
+						if (collisionMonitorInfo.timeFromStartCollision > halfDT &&
+							timeSinceCollisionStart <= startInterpolatingBack &&
+							collisionMonitorInfo.relativeDistance < 1.2f)
+						{
+#if MRE_PHYSICS_DEBUG
+							Debug.Log(" unconditionally add to collision stream time:" + timeSinceCollisionStart +
+								" relative dist:" + collisionMonitorInfo.relativeDistance);
+#endif
+							addToMonitor = true;
+						}
+						// switch over smoothly to the key framing
+						if (!addToMonitor && timeSinceCollisionStart > startInterpolatingBack && timeSinceCollisionStart <= endInterpolatingBack)
+						{
+							addToMonitor = true;
+							float oldVal = collisionMonitorInfo.keyframedInterpolationRatio;
+							// progress the interpolation
+							collisionMonitorInfo.keyframedInterpolationRatio =
+								invInterpolationTimeWindows * (timeSinceCollisionStart - startInterpolatingBack);
+#if MRE_PHYSICS_DEBUG
+							Debug.Log(" doing interpolation new:" + collisionMonitorInfo.keyframedInterpolationRatio +
+								" old:" + oldVal);
+#endif
+							// stop interpolation
+							if (collisionMonitorInfo.relativeDistance < 0.8f
+								&& collisionMonitorInfo.keyframedInterpolationRatio > 0.2f)
+							{
+#if MRE_PHYSICS_DEBUG
+								Debug.Log(" Stop interpolation time with DT:" + DT +
+									" Ratio:" + collisionMonitorInfo.keyframedInterpolationRatio +
+									" relDist:" + collisionMonitorInfo.relativeDistance);
+#endif
+								collisionMonitorInfo.timeFromStartCollision -= DT;
+							}
+						}
+
+						// we add to the collision stream either when it is within range or 
+						if (isWithinCollisionRange || addToMonitor)
+						{
+							// add to the monitor stream 
+							_monitorCollisionInfo.Add(remoteBodyInfo.rigidBodyId, collisionMonitorInfo);
+
+							// this is a new collision
+							if (collisionMonitorInfo.timeFromStartCollision < halfDT)
+							{
+								// switch back to dynamic
+								remoteBody.isKinematic = false;
+								remoteBody.transform.position = remoteBodyInfo.startPosition;
+								remoteBody.transform.rotation = remoteBodyInfo.startOrientation;
+								remoteBody.velocity = remoteBodyInfo.linearVelocity;
+								remoteBody.angularVelocity = remoteBodyInfo.angularVelocity;
+#if MRE_PHYSICS_DEBUG
+								Debug.Log(" remote body velocity SWITCH collision: " + remoteBody.velocity.ToString() +
+	                               "  start position:" + remoteBody.transform.position.ToString());
+#endif
+							}
+							else
+							{
+#if MRE_PHYSICS_DEBUG
+								// this is a previous collision so do nothing just leave dynamic
+								Debug.Log(" remote body velocity stay collision: " + remoteBody.velocity.ToString() +
+									"  start position:" + remoteBody.transform.position.ToString() +
+									" relative dist:" + collisionMonitorInfo.relativeDistance +
+									" Ratio:" + collisionMonitorInfo.keyframedInterpolationRatio );
+#endif
+							}
+						}
+					}
+				}
+			}
+
 		}
 
 		public Snapshot Update(UnityEngine.Transform rootTransform)
@@ -193,9 +422,9 @@ namespace MixedRealityExtension.Core
 			// and generate update packet/snapshot
 
 			Snapshot snapshot = new Snapshot();
-			snapshot.SourceId = _appId;
+			snapshot.SourceAppId = _appId;
 			snapshot.Time = UnityEngine.Time.fixedTime + UnityEngine.Time.fixedDeltaTime;
-			snapshot.Transforms.Capacity = _countOwnedTransforms;
+			snapshot.snapshotTransforms.Capacity = _countOwnedTransforms;
 
 			foreach (var rb in _rigidBodies.Values)
 			{
@@ -204,7 +433,7 @@ namespace MixedRealityExtension.Core
 					continue;
 				}
 
-				var ti = new Snapshot.TrasformInfo();
+				var ti = new Snapshot.SnapshotTrasformInfo();
 				ti.Id = rb.Id;
 
 				//Position = new Vector3Patch(App.SceneRoot.transform.InverseTransformPoint(transform.position)),
@@ -213,10 +442,10 @@ namespace MixedRealityExtension.Core
 				//ti.Transform.Position = rb.RigidBody.transform.position - rootTransform.position;
 				//ti.Transform.Rotation = UnityEngine.Quaternion.Inverse(rootTransform.rotation) * rb.RigidBody.transform.rotation;
 
-				ti.Transform.Position = rootTransform.InverseTransformPoint(rb.RigidBody.transform.position);
-				ti.Transform.Rotation = UnityEngine.Quaternion.Inverse(rootTransform.rotation) * rb.RigidBody.transform.rotation;
+				ti.sTransform.Position = rootTransform.InverseTransformPoint(rb.RigidBody.transform.position);
+				ti.sTransform.Rotation = UnityEngine.Quaternion.Inverse(rootTransform.rotation) * rb.RigidBody.transform.rotation;
 
-				snapshot.Transforms.Add(ti);
+				snapshot.snapshotTransforms.Add(ti);
 			}
 
 			return snapshot;
