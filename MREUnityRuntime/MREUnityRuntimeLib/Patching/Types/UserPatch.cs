8ddc296e1189367d8de1952c49512ab9eb1dff4b
Steven Vergenz <steven.vergenz@microsoft.com>
2020-07-08 10:33:35 -0700

    Report granted permissions in the user-joined message

--- MREUnityRuntime/MREUnityRuntimeLib/Patching/Types/UserPatch.cs
+++ MREUnityRuntime/MREUnityRuntimeLib/Patching/Types/UserPatch.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 using MixedRealityExtension.Animation;
+using MixedRealityExtension.API;
 using MixedRealityExtension.Core;
 using MixedRealityExtension.Core.Types;
 using MixedRealityExtension.Messaging.Payloads.Converters;
@@ -8,6 +9,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MixedRealityExtension.Patching.Types
 {
@@ -21,6 +23,9 @@ namespace MixedRealityExtension.Patching.Types
 		[PatchProperty]
 		[JsonConverter(typeof(UnsignedConverter))]
 		public UInt32? Groups { get; set; }
+
+		[PatchProperty]
+		public Permissions[] GrantedPermissions { get; set; }
 
 		public Dictionary<string, string> Properties { get; set; }
 
@@ -38,6 +43,8 @@ namespace MixedRealityExtension.Patching.Types
 		{
 			Name = user.Name;
 			Groups = user.Groups;
+			// the server doesn't need to care about the execution permission, it's assumed if you're connected
+			GrantedPermissions = user.App.GrantedPermissions.ToEnumerable().Where(p => p != Permissions.Execution).ToArray();
 			Properties = user.UserInfo.Properties;
 		}
 	}
