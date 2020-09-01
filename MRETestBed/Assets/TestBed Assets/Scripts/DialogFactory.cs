24eae39a482f59fc8e22a7e54b8af13c000d44f1
Pat O'Leary (ALTSPACE) <patole@microsoft.com>
2020-09-01 15:45:52 -0700

    Enable text-entry popups in testbed

--- MRETestBed/Assets/TestBed Assets/Scripts/DialogFactory.cs
+++ MRETestBed/Assets/TestBed Assets/Scripts/DialogFactory.cs
@@ -7,14 +7,26 @@ using UnityEngine.UI;
 using MixedRealityExtension.App;
 using MixedRealityExtension.PluginInterfaces;
 using System;
+using UnityEditor;
+using MixedRealityExtension.Assets;
 
 public class DialogFactory : MonoBehaviour, IDialogFactory
 {
-	[SerializeField] private Canvas canvas;
-	[SerializeField] private UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller;
+
+	private GameObject baseCanvasGameObject;
+	private GameObject activeCanvasGameObject;
+	[SerializeField] public UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller;
 	[SerializeField] private Assets.Scripts.User.InputSource inputSource;
-	[SerializeField] private Text label;
-	[SerializeField] private InputField input;
+	private Canvas activeCanvas;
+	private Text activePromptText;
+	private InputField activeInputField;
+	private Text activeInputText;
+	private GameObject cancelButton;
+	private GameObject submitButton;
+	private GameObject okButton;
+	private Button cancelButtonScript;
+	private Button submitButtonScript;
+	private Button okButtonScript;
 
 	private class DialogQueueEntry
 	{
@@ -26,38 +38,93 @@ public class DialogFactory : MonoBehaviour, IDialogFactory
 	private Queue<DialogQueueEntry> queue = new Queue<DialogQueueEntry>(3);
 	private DialogQueueEntry activeDialog;
 
+	private void Update()
+	{
+		if (activeCanvas != null && activeCanvas.enabled && Input.GetKeyDown(KeyCode.Return))
+		{
+			OnSubmit();
+		}
+
+	}
 	private void Start()
 	{
+		if (activeCanvasGameObject == null)
+		{
+			//baseCanvasGameObject = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Testbed Assets/Prefabs/PromptTemplate.prefab", typeof(GameObject));
+			activeCanvasGameObject = this.transform.gameObject;
+			activeCanvas = (Canvas)activeCanvasGameObject.GetComponent("Canvas");
+			activeCanvas.enabled = false;
+			activePromptText = (Text)activeCanvasGameObject.transform.Find("PromptText").GetComponent("Text");
+			activeInputField = (InputField)activeCanvasGameObject.transform.Find("PromptInput").GetComponent<InputField>();
+			activeInputField.enabled = true;
+			activeInputText = (Text)activeCanvasGameObject.transform.Find("PromptInput").transform.Find("Text").GetComponent("Text");
+
+			cancelButton = activeCanvasGameObject.transform.Find("CancelButton").gameObject;
+			cancelButtonScript = (Button)cancelButton.GetComponent("Button");
+			cancelButtonScript.onClick.AddListener(OnCancel);
+			submitButton = activeCanvasGameObject.transform.Find("SubmitButton").gameObject;
+			submitButtonScript = (Button)submitButton.GetComponent("Button");
+			submitButtonScript.onClick.AddListener(OnSubmit);
+			okButton = activeCanvasGameObject.transform.Find("OKButton").gameObject;
+			okButtonScript = (Button)okButton.GetComponent("Button");
+			okButtonScript.onClick.AddListener(OnOk);
+		}
 		Hide();
 	}
 
 	public void ShowDialog(IMixedRealityExtensionApp app, string text, bool acceptInput, Action<bool, string> callback)
-	{
+	{ 
 		queue.Enqueue(new DialogQueueEntry() { text = text, allowInput = acceptInput, callback = callback });
+
+		if(acceptInput)
+		{
+			okButton.SetActive(false);
+			submitButton.SetActive(true);
+			cancelButton.SetActive(true);
+		}
+		else
+		{
+			okButton.SetActive(true);
+			submitButton.SetActive(false);
+			cancelButton.SetActive(false);
+		}
+
 		ProcessQueue();
 	}
 
 	private void ProcessQueue()
 	{
-		if (queue.Count == 0 || canvas.enabled) return;
+		if (queue.Count == 0 || activeCanvas.enabled) return;
 
 		activeDialog = queue.Dequeue();
-		label.text = activeDialog.text;
-		input.gameObject.SetActive(activeDialog.allowInput);
-		input.text = "";
+		activePromptText.text = activeDialog.text;
+		activeInputField.ActivateInputField();
+		activeInputField.text = "";
 
-		canvas.enabled = true;
-		controller.enabled = false;
-		inputSource.enabled = false;
+		if(activeCanvas)
+			activeCanvas.enabled = true;
+		if(controller)
+			controller.enabled = false;
+
+		//inputSource.enabled = false;
 		Cursor.lockState = CursorLockMode.None;
 		Cursor.visible = true;
 	}
 
 	private void OnOk()
 	{
+		activeDialog = null;
+		Hide();
+	}
+
+	private void OnSubmit()
+	{
 		try
 		{
-			activeDialog.callback?.Invoke(true, activeDialog.allowInput ? input.text : null);
+			string text = activeInputField.text;
+			activeDialog.callback?.Invoke(true, text);
+			activeInputField.text = "";
+			activeInputField.enabled = false;
 		}
 		catch (Exception e)
 		{
@@ -76,6 +143,8 @@ public class DialogFactory : MonoBehaviour, IDialogFactory
 		try
 		{
 			activeDialog.callback?.Invoke(false, null);
+			activeInputField.text = "";
+			activeInputField.enabled = false;
 		}
 		catch (Exception e)
 		{
@@ -91,9 +160,14 @@ public class DialogFactory : MonoBehaviour, IDialogFactory
 
 	private void Hide()
 	{
-		canvas.enabled = false;
-		controller.enabled = true;
-		inputSource.enabled = true;
+		if(activeCanvas)
+			activeCanvas.enabled = false;
+		if(controller)
+			controller.enabled = true;
+
+		
+
+		//inputSource.enabled = true;
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 
